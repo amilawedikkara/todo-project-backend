655ab8908fdb4d3abd189cdfd1dc49da2be9dac8
amilai <amila.wedikkara@gmail.com>
2025-03-23 10:55:27 +0200

    update app.use/, todoRouter

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
-const todoRoutes = require("./routes/todo.js");
+const todoRouter = require("./routes/todo.js");
 
 const app = express();
 app.use(cors());
 app.use(express.json()); // Allow parsing of JSON bodies in requests
 app.use(express.urlencoded({ extended: false })); // Allow parsing of URL-encoded bodies in requests
-app.use("/", todoRoutes); // todos or tasks?
+app.use("/", todoRouter); // todos or tasks?
 const port = process.env.PORT || 3001;
 
 app.listen(port, () => {
